20e4ac6bad1aefbe96431065ccbf26aa60345ab5
hyune0129™ hyune0129™ <hyune0129@naver.com>
2022-11-30 04:06:03 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+
+public class Main {
+	public static void main(String args[]) {
+		String str = "{\"IS_ADMIN\":false,\"PASSWORD\":\"sksmsthskxk\",\"MID\":\"IMSNT\",\"EMAIL\":\"asdf2@gmail.com\",\"NAME\":\"나는소나타\",\"CID\":2}";
+	}
+}
